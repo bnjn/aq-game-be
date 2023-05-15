66b4e24376603144dae8960aea0d062dee5a5480
benjamin.johnson <benjamin.johnson@madetech.com>
2023-05-15 16:10:30 +0100

    test for country list api call

---
+++ server.test.ts
@@ -0,0 +1,31 @@
+const server = require('./server');
+import axios from 'axios';
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock('axios');
+
+describe('getCountryList', () => {
+  const mockCountryData : object = {
+    "status": "success",
+    "data": [
+      {
+        "country": "Andorra"
+      },
+      {
+        "country": "Argentina"
+      },
+      {
+        "country": "Australia"
+      },
+      {
+        "country": "Austria"
+      }
+    ]
+  }
+
+  it('returns a list of available countries from AirVisual API', () => {
+    mockedAxios.get.mockResolvedValue(mockCountryData);
+    const country : string = server.getCountryList();
+    expect(country).toContain('Australia');
+  });
+});
