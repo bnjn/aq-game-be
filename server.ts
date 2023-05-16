7778e432bda8fc0d217b6c6303dee242a32f533c
benjamin.johnson <benjamin.johnson@madetech.com>
2023-05-16 15:14:24 +0100

    get request to airvisual api for country list

--- server.ts
+++ server.ts
@@ -1,3 +1,11 @@
-export function getCountryList() : string[] {
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function getCountryList() : Promise<string[]> {
+
+    const response = await axios.get(`http://api.airvisual.com/v2/countries?key=${process.env.AIRVISUAL_API_KEY}`);
+
     return ['Australia', 'Argentina'];
 }
