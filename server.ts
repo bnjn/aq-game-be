df9d972484ab230bb1f72e55264646c38e34c12d
benjamin.johnson <benjamin.johnson@madetech.com>
2023-05-16 15:55:33 +0100

    getCountryList handles API error

--- server.ts
+++ server.ts
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export async function getCountryList() : Promise<string[]> {
-
-    const response = await axios.get(`http://api.airvisual.com/v2/countries?key=${process.env.AIRVISUAL_API_KEY}`);
-
-    return ['Australia', 'Argentina'];
+    const response = await axios.get(`https://api.airvisual.com/v2/countries?key=${process.env.AIRVISUAL_API_KEY}`);
+    if (response.status.toString() !== 'success') {
+        throw new Error(`Error status from AirVisual API: ${response.status.toString()}`);
+    }
+    return response.data.map((country: { country: object }) => country.country);
 }
