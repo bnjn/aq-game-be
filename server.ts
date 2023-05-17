82a33f9c828a508d4d0b12a1022de813880ce2c9
benjamin.johnson <benjamin.johnson@madetech.com>
2023-05-17 15:18:57 +0100

    tests for getStateList passing. now handles resolved and rejected promises correctly as well as api errors.

--- server.ts
+++ server.ts
@@ -11,6 +11,18 @@ export async function getCountryList() : Promise<string[]> {
     return response.data.map((country: { country: object }) => country.country);
 }
 
-export async function getStateList(country: string) : Promise<string[]> {
-    return ['Beijing'];
+
+export async function getStateList(country: string) : Promise<any> {
+    try {
+        const response = await axios.get(`https://api.airvisual.com/v2/states?country=${country}&key=${process.env.AIRVISUAL_API_KEY}`);
+        return Promise.resolve(response.data.data.map((state: { state: object }) => state.state));
+    } catch (error: any) {
+        if (error.response.data.status === 'fail') {
+                if (error.response.data.data.message === 'country_not_found') {
+                    return Promise.reject(new Error(`AirVisual API: country not found "${country}"`));
+                } else {
+                    return Promise.reject(new Error(`Error status from AirVisual API: ${error.response.data.data.message}`));
+                }
+        }
+    }
 }
