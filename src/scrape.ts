56901fabc47c0dc0bbf473cd17dcae8610f40e45
benjamin.johnson <benjamin.johnson@madetech.com>
2023-06-27 11:39:15 +0100

    saveStateData only queries existing and new countries

--- src/scrape.ts
+++ src/scrape.ts
@@ -15,87 +15,85 @@ export async function saveCountryData(): Promise<void> {
     }).catch(e => console.log(e))
 }
 
-export async function saveStateData(): Promise<void> {
-    if (fs.existsSync('./data/countryData.json')) {
-        let states : object[] = [];
-
-        fs.readFile('./data/countryData.json', 'utf8', (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-
-            const worker = function(country: string, done: Function) {
-                console.log('Processing', country, '@', new Date().getTime());
-                getStateList(country).then((data) => {
-                    states.push({
-                        country: country,
-                        last_updated: new Date(),
-                        states: data
-                    });
-                    fs.writeFileSync("./data/stateData.json", JSON.stringify(states), 'utf-8');
-                })
-                    .catch(e => console.log(e));
-                // Max 5 calls per minute
-                setTimeout(done, 13000);
-            }
-
-            const queue = qrate(worker, 1, 1);
-
-            const countries = JSON.parse(data).countries;
-            countries.forEach((country: string) => {
-                queue.push(country);
+export async function saveStateData(countries: string[]): Promise<void> {
+    let states : object[] = [];
+    const worker = function(country: string, done: Function) {
+        getStateList(country).then((data) => {
+            states.push({
+                country: country,
+                last_updated: new Date(),
+                states: data
             });
+            fs.writeFileSync("./data/states.json", JSON.stringify(states), 'utf-8');
+        })
+            .catch(e => console.log(e));
+        // Max 5 calls per minute
+        setTimeout(done, 13000);
+    }
 
-
+    if (fs.existsSync('./data/states.json')) {
+        const queue = qrate(worker, 1, 1);
+        const existingCountries = JSON.parse(fs.readFileSync('./data/states.json', 'utf8')).map((country: any) => country.country);
+        const allCountries = new Set([...countries, ...existingCountries]);
+        [...allCountries].forEach((country) => {
+            console.log('Processing', country, '@', new Date().getTime());
+            queue.push(country)
         });
     } else {
-        saveCountryData().then(() => saveStateData()).catch(e => console.log(e));
-    }
-}
-
-export async function saveCityData(): Promise<void> {
-    // TODO: tests for state not found and too many requests.
-    if (fs.existsSync('./stateData.json')) {
-        let cities : object[] = [];
-
-        fs.readFile('./data/stateData.json', 'utf8', (err, data) => {
-            if (err) {
-                console.log(err);
-            }
-
-            const worker = function(state: any, done: Function) {
-                console.log('Processing', state.country, state.name, '@', new Date().getTime());
-                getCityList(state.country, state.name).then((data) => {
-                    cities.push({
-                        country: state.country,
-                        last_updated: new Date(),
-                        state: state.name,
-                        cities: data
-                    });
-                    fs.writeFileSync("./data/cityData.json", JSON.stringify(cities), 'utf-8');
-                })
-                    .catch(e => console.log(e));
-                // Max 5 calls per minute
-                setTimeout(done, 13000);
-            }
-
-            const queue = qrate(worker, 1, 1);
-
-            const countries: Array<any> = JSON.parse(data);
-            countries.forEach((country: any) => {
-                if (country.country === 'United Kingdom') {
-                    country.states.forEach((state: any) => {
-                        queue.push({country: country.country, name: state});
-                    });
-                }
-            });
-
-
+        const queue = qrate(worker, 1, 1);
+        countries.forEach((country) => {
+            console.log('Processing', country, '@', new Date().getTime());
+            queue.push(country)
         });
-    } else {
-        saveStateData().then(() => saveCityData()).catch(e => console.log(e));
     }
+
+
+
 }
+
+// export async function saveCityData(): Promise<void> {
+//     // TODO: tests for state not found and too many requests.
+//     if (fs.existsSync('./stateData.json')) {
+//         let cities : object[] = [];
+//
+//         fs.readFile('./data/stateData.json', 'utf8', (err, data) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//
+//             const worker = function(state: any, done: Function) {
+//                 console.log('Processing', state.country, state.name, '@', new Date().getTime());
+//                 getCityList(state.country, state.name).then((data) => {
+//                     cities.push({
+//                         country: state.country,
+//                         last_updated: new Date(),
+//                         state: state.name,
+//                         cities: data
+//                     });
+//                     fs.writeFileSync("./data/cityData.json", JSON.stringify(cities), 'utf-8');
+//                 })
+//                     .catch(e => console.log(e));
+//                 // Max 5 calls per minute
+//                 setTimeout(done, 13000);
+//             }
+//
+//             const queue = qrate(worker, 1, 1);
+//
+//             const countries: Array<any> = JSON.parse(data);
+//             countries.forEach((country: any) => {
+//                 if (country.country === 'United Kingdom') {
+//                     country.states.forEach((state: any) => {
+//                         queue.push({country: country.country, name: state});
+//                     });
+//                 }
+//             });
+//
+//
+//         });
+//     } else {
+//         saveStateData().then(() => saveCityData()).catch(e => console.log(e));
+//     }
+// }
 
 // Save data
 // saveCountryData()
