23785a7727a7481dac94454147adfa3400a33dba
benjamin.johnson <benjamin.johnson@madetech.com>
2023-06-27 11:28:08 +0100

    saves state data if file not found on server launch

--- src/start.ts
+++ src/start.ts
@@ -1,6 +1,22 @@
 import app from './server';
 import dotenv from 'dotenv';
+import fs from "fs";
+import {saveStateData} from "./scrape";
 
 dotenv.config()
-app.listen(process.env.PORT);
-console.log(`Air Quality API started on port ${process.env.PORT}`)
+
+const port = process.env.PORT ?? 3000;
+
+if (fs.existsSync('./data/states.json')) {
+    console.log('State data found at data/states.json');
+    app.listen(port);
+    console.log(`Air Quality API started on port ${process.env.PORT}`)
+} else {
+    saveStateData([process.env.COUNTRY ?? 'United Kingdom']).then(() => {
+        console.log(`State data written to data/states.json`);
+    }).finally(() => {
+        app.listen(port);
+        console.log(`Air Quality API started on port ${process.env.PORT}`)
+    }).catch((err) => { throw err});
+}
+
