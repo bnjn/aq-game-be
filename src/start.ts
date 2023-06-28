b1dc17a3ad703312ed0c17533f2773f45d327e8c
benjamin.johnson <benjamin.johnson@madetech.com>
2023-06-28 15:57:21 +0100

    moved data scraping code from scrape.ts to start.ts, now runs on server start. all data now in cities.json

--- src/start.ts
+++ src/start.ts
@@ -1,22 +1,44 @@
 import app from './server';
 import dotenv from 'dotenv';
 import fs from "fs";
-import {saveStateData} from "./scrape";
+const qrate = require('qrate');
+import {getCityList, getStateList} from "./api";
 
 dotenv.config()
 
 const port = process.env.PORT ?? 3000;
 
-if (fs.existsSync('./data/states.json')) {
-    console.log('State data found at data/states.json');
-    app.listen(port);
-    console.log(`Air Quality API started on port ${process.env.PORT}`)
-} else {
-    saveStateData([process.env.COUNTRY ?? 'United Kingdom']).then(() => {
-        console.log(`State data written to data/states.json`);
-    }).finally(() => {
+function start() {
+    let cities: object[] = [];
+    const worker = function (state: any, done: Function) {
+        console.log('Processing', state, '@', new Date().getTime());
+        getCityList(state.country, state.state).then((data) => {
+            data.forEach((city: string) => {
+                cities.push({
+                    country: state.country,
+                    last_updated: new Date(),
+                    state: state.state,
+                    city: city
+                });
+            });
+            fs.writeFileSync("./data/cities.json", JSON.stringify(cities), 'utf-8');
+            setTimeout(done,15000);
+        });
+
+    }
+    const queue = qrate(worker, 1, 1);
+    getStateList('United Kingdom' ?? process.env.COUNTRY).then((states) => {
+        states.forEach((state: string) => {
+            queue.push({state: state, country: 'United Kingdom' ?? process.env.COUNTRY})
+        });
+    });
+
+    queue.drain = () => {
+        // all of the queue items have been processed
+        console.log(`Data written to data/cities.json`);
         app.listen(port);
         console.log(`Air Quality API started on port ${process.env.PORT}`)
-    }).catch((err) => { throw err});
+    }
 }
 
+start();
